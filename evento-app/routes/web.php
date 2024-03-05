f0604b4b8e51e38a8e2543fb75bb080d165affa6
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-03-05 22:18:59 +0100

    login done

--- evento-app/routes/web.php
+++ evento-app/routes/web.php
@@ -1,18 +1,37 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
+//use App\Http\Controllers\LoginController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ForgotPasswordLinkController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('logout', [LogoutController::class, 'destroy'])
+    ->middleware('auth');
+Route::get("register", [RegisterController::class, 'create'])->name('Form-register');
+
+Route::post("register", [RegisterController::class, 'store'])->name('register');
+
+Route::get("login", [LoginController::class, 'create'])->name('Form-login');
+
+Route::post("login", [LoginController::class, 'authenticate'])->name('login');
+Route::middleware('guest')->group(function () {
+
+
+    Route::get('/request', [ForgotPasswordLinkController::class, 'create']);
+
+    Route::post('/request', [ForgotPasswordLinkController::class, 'store']);
+
+    Route::get('password/reset/{token}', [ForgotPasswordController::class, 'create'])->name('password.reset');
+
+    Route::post('/reset', [ForgotPasswordController::class, 'reset'])->name('reset');
 });
