5dc66c6ebeee982e6f137b89fb30845935f822d1
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-03-06 11:15:10 +0100

    change the front of login and add the possibility to sign with google

--- evento-app/resources/views/Auth/login.blade.php
+++ evento-app/resources/views/Auth/login.blade.php
@@ -9,61 +9,129 @@
 </head>
 
 <body>
-<section class="bg-blue-50 dark:bg-gray-900">
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div
-            class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1
-                    class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Sign in to your account
-                </h1>
-                <form class="space-y-4 md:space-y-6" method="post" action="{{ route('login') }}">
-                    @csrf
-                    <div>
-                        <label for="email"
-                               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                        <input type="email" name="email" id="email"
-                               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                               placeholder="name@company.com" required="">
-                    </div>
-                    <div>
-                        <label for="password"
-                               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••"
-                               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                               required="">
-                        @error('login')
-                        <span class="text-red-500">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-start">
-                            <div class="flex items-center h-5">
-                                <input id="remember" name="remember" aria-describedby="remember" type="checkbox"
-                                       class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
-                            </div>
-                            <div class="ml-3 text-sm">
-                                <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                            </div>
-                        </div>
-                        <a href="/request"
-                           class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
-                            password?</a>
 
-                    </div>
-                    <button type="submit"
-                            class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign
-                        in</button>
-                    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                        Don’t have an account yet? <a href="{{ route('Form-register') }}"
-                                                      class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                    </p>
-                </form>
-            </div>
+<!-- component -->
+<!-- Simple login form -->
+
+<div class="bg-white sm:bg-gray-200 min-h-screen w-screen flex flex-col justify-center items-center">
+    <div
+        class="bg-white shadow-none sm:shadow-lg px-8 sm:px-12 w-full xs:w-full sm:w-8/12 md:w-7/12 lg:w-7/12 xl:w-2/6 h-screen sm:h-auto py-8">
+        <div class="text-center w-full font-bold text-3xl text-gray-600 p-4">
+            LOGIN
         </div>
+        <div
+            class="w-full bg-gray-200 my-3" style="height: 1px"
+        ></div>
+        <form method="post" action="{{ route('login') }}">
+            @csrf
+            <div class="flex flex-col gap-4 px-0 py-4">
+                <div>
+                    <label class="text-gray-700">Email address</label>
+                    <!-- <AtSymbolIcon class="font-medium text-2xl text-gray-600 absolute p-2.5 px-3 w-11" /> -->
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                         class="font-medium text-2xl text-gray-600 absolute p-2.5 px-3 w-11" fill="none"
+                         viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"/>
+                    </svg>
+                    <input
+                        class="py-2 pl-10 border border-gray-200 w-full"
+                        placeholder="Email address"
+                        type="email"
+                    />
+                </div>
+                <div>
+                    <label class="text-gray-700">Password</label>
+                    <!-- <LockClosedIcon class="font-medium text-2xl text-gray-600 absolute p-2.5 px-3 w-11" /> -->
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                         class="font-medium text-2xl text-gray-600 absolute p-2.5 px-3 w-11" fill="none"
+                         viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/>
+                    </svg>
+                    <input
+                        class="py-2 pl-10 border border-gray-200 w-full"
+                        placeholder="Password"
+                        type="password"
+                    />
+                    @error('login')
+                    <span class="text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="w-full flex flex-row gap-2">
+                    <button
+                        class="border border-indigo-500 hover:bg-indigo-500 hover:text-white duration-100 ease-in-out w-6/12 text-indigo-500 p-0 flex flex-row justify-center items-center gap-1"
+                        type="submit"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"/>
+                        </svg>
+                        Login
+                    </button>
+                    <button
+                        class="border border-indigo-500 hover:bg-indigo-500 hover:text-white duration-100 ease-in-out w-6/12 text-indigo-500 p-2 flex flex-row justify-center items-center gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
+                        </svg>
+                        <a href="{{ route('Form-register') }}">Sign-up</a>
+                    </button>
+                </div>
+                <div class="my-2 flex flex-row justify-center">
+                    <span class="absolute bg-white px-4">or</span>
+                    <div
+                        class="w-full bg-gray-200 mt-3" style="height: 1px"
+                    ></div>
+                </div>
+                <div class="w-full flex flex-col gap-2">
+                    <button
+                        class="bg-red-500 text-white w-full p-2 flex flex-row justify-center gap-2 items-center hover:bg-red-600 duration-100 ease-in-out">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                             aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet"
+                             viewBox="0 0 24 24">
+                            <g fill="none">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                      d="M12 0C5.372 0 0 5.373 0 12s5.372 12 12 12c6.627 0 12-5.373 12-12S18.627 0 12 0zm.14 19.018c-3.868 0-7-3.14-7-7.018c0-3.878 3.132-7.018 7-7.018c1.89 0 3.47.697 4.682 1.829l-1.974 1.978v-.004c-.735-.702-1.667-1.062-2.708-1.062c-2.31 0-4.187 1.956-4.187 4.273c0 2.315 1.877 4.277 4.187 4.277c2.096 0 3.522-1.202 3.816-2.852H12.14v-2.737h6.585c.088.47.135.96.135 1.474c0 4.01-2.677 6.86-6.72 6.86z"
+                                      fill="currentColor"/>
+                            </g>
+                        </svg>
+                        Sign-in with Google
+                    </button>
+                    <button
+                        class="bg-blue-600 text-white w-full p-2 flex flex-row justify-center gap-2 items-center hover:bg-blue-700 duration-100 ease-in-out">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                             aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet"
+                             viewBox="0 0 1024 1024">
+                            <path
+                                d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zm-92.4 233.5h-63.9c-50.1 0-59.8 23.8-59.8 58.8v77.1h119.6l-15.6 120.7h-104V912H539.2V602.2H434.9V481.4h104.3v-89c0-103.3 63.1-159.6 155.3-159.6c44.2 0 82.1 3.3 93.2 4.8v107.9z"
+                                fill="currentColor"/>
+                        </svg>
+                        Sign-in with Facebook
+                    </button>
+                    <button
+                        class="bg-gray-700 text-white w-full p-2 flex flex-row justify-center gap-2 items-center hover:bg-gray-800 duration-100 ease-in-out">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                             aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet"
+                             viewBox="0 0 24 24">
+                            <g fill="none">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                      d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z"
+                                      fill="currentColor"/>
+                            </g>
+                        </svg>
+                        Sign-in with Github
+                    </button>
+                </div>
+                <div class="w-full flex flex-row justify-end">
+                    <a href="#">Forgot password</a>
+                </div>
+            </div>
+        </form>
     </div>
-</section>
-</body>
+</div>
 
+</body>
 </html>
