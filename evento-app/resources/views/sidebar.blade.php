3b48ae211316c634376645880d5d75098e8b26fb
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-03-06 11:15:42 +0100

    add sidebar for organizer

--- evento-app/resources/views/sidebar.blade.php
+++ evento-app/resources/views/sidebar.blade.php
@@ -131,6 +131,27 @@
                     <span class="ml-2 text-sm tracking-wide truncate">Notifications</span>
                 </a>
             </li>
+        </ul>
+        <ul class="flex flex-col py-4 space-y-1">
+            <li class="px-5 hidden md:block">
+                <div class="flex flex-row items-center h-8">
+                    <div class="text-sm font-light tracking-wide text-gray-400 uppercase">Organizer</div>
+                </div>
+            </li>
+            <li>
+                <a href="/addevent"
+                   class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
+                                <span class="inline-flex justify-center items-center ml-4">
+                                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                       xmlns="http://www.w3.org/2000/svg">
+                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+                                      </path>
+                                  </svg>
+                                </span>
+                    <span class="ml-2 text-sm tracking-wide truncate">Add Event</span>
+                </a>
+            </li>
 
         </ul>
         <p class="mb-14 px-5 py-3 hidden md:block text-center text-xs">Copyright @2021</p>
