1b99004b299a17f4f4d18f6581e1844670dd950e
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-03-07 10:20:07 +0100

    event model

--- evento-app/app/Models/Event.php
+++ evento-app/app/Models/Event.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'location',
+        'date',
+        'time',
+        'price',
+        'nbr_place',
+        'description',
+        'reservation_type',
+        'image',
+        'creator'
+    ];
 }
