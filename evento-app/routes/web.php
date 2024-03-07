e3ea6b7462d12007235d89f1397ec83609e36054
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-03-07 10:22:28 +0100

    route for event

--- evento-app/routes/web.php
+++ evento-app/routes/web.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\LoginController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\ForgotPasswordLinkController;
 use App\Http\Controllers\Auth\LogoutController;
@@ -48,9 +48,8 @@ Route::post("register", [RegisterController::class, 'store'])->name('register');
 Route::get("login", [LoginController::class, 'create'])->name('Form-login');
 
 Route::post("login", [LoginController::class, 'authenticate'])->name('login');
+
 Route::middleware('guest')->group(function () {
-
-
     Route::get('/request', [ForgotPasswordLinkController::class, 'create']);
 
     Route::post('/request', [ForgotPasswordLinkController::class, 'store']);
@@ -68,3 +67,15 @@ Route::post('/dashboard_category', [CategoryController::class, 'store']);
 Route::delete('/deleteCategory/{id}', [CategoryController::class, 'destroyCategory']);
 Route::delete('/destroy/{id}', [CategoryController::class, 'destroy']);
 
+//events
+Route::get('/createEvent', [EventController::class, 'showForm']);
+
+Route::post('/createEvent', [EventController::class, 'store']);
+
+Route::get('/allEvents', [EventController::class, 'AllEvents']);
+
+Route::delete('/deleteEvent/{id}', [EventController::class, 'deleteEvent']);
+
+Route::get('/updateEvent/{id}', [EventController::class, 'editEvent']);
+
+Route::post('/updateEvent/{id}', [EventController::class, 'updateEvent']);
